b4df3b8d92f6d491d2fc296bb273bebd97ad1bb5
resteem <eremeevst@gmail.com>
2013-08-04 21:53:52 +0400

    Create new console application project.

---
+++ prjShotMonitor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prjShotMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
